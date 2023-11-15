be31372d98e5fa77bcf56ff95c084fc8c2dc213c
Kazuhide Adachi <kazuhide.adachi@gmail.com>
2023-11-15 18:46:10 +0900

    Add weather view

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,86 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: WeatherView(),
         ),
       ),
+    );
+  }
+}
+
+class WeatherView extends StatelessWidget {
+  const WeatherView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const SizedBox(height: 80 + 48),
+        Flexible(
+          child: SizedBox(
+            width: width / 2,
+            height: width / 2,
+            child: const Placeholder(),
+          ),
+        ),
+        Flexible(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              maxWidth: width / 2,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      '** ℃',
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                            color: Colors.blue,
+                          ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      '** ℃',
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                            color: Colors.red,
+                          ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(height: 80),
+        ConstrainedBox(
+          constraints: BoxConstraints(
+            maxWidth: width / 2,
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text('Close'),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text('Reload'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
